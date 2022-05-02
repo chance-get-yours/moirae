ec2f9b7f3c45439057c611a23d3e55b91677dd3c
jes3rk <joseph.schefer@gmail.com>
2022-05-02 18:59:42 +0200

    skeleton setup

---
+++ packages/template/jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+const fs = require("fs");
+
+const config = JSON.parse(fs.readFileSync(`${__dirname}/../../.swcrc`, "utf-8"));
+
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "lib",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": ["@swc/jest", { ...config }]
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["./jest.setup.js"]
+};
---
+++ packages/typeorm-store/lib/smoke.spec.ts
@@ -0,0 +1,5 @@
+describe("Smoke Test", () => {
+  it("will add numbers", () => {
+    expect(2 + 2).toEqual(4);
+  });
+});

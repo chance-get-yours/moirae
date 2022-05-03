9caf68d6fa89edb318fdd3ea251fa3e6977f6678
jes3rk <joseph.schefer@gmail.com>
2022-05-03 09:46:05 +0200

    fix: remove duplicate jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const fs = require("fs");
 
-const config = JSON.parse(fs.readFileSync(`${__dirname}/../../.swcrc`, "utf-8"));
+const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, "utf-8"));
 
 module.exports = {
   moduleFileExtensions: ["js", "json", "ts"],

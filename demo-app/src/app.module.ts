067dc7fd686cb0dc6f3513d08fa730df54a9f578
jes3rk <joseph.schefer@gmail.com>
2022-05-20 15:21:06 +0200

    demo: update config for typeorm module

--- demo-app/src/app.module.ts
+++ demo-app/src/app.module.ts
@@ -1,5 +1,11 @@
-import { IMoiraeConfig, IPublisherConfig, MoiraeModule } from "@moirae/core";
+import {
+  IMoiraeConfig,
+  IPublisherConfig,
+  IStoreConfig,
+  MoiraeModule,
+} from "@moirae/core";
 import { IRabbitMQConfig } from "@moirae/rabbitmq-publisher";
+import { EventStore, ITypeORMStoreConfig } from "@moirae/typeorm-store";
 import { Module, NotFoundException } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
@@ -8,8 +14,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MoiraeWsGateway } from "./moirae-ws.gateway";
 
-const moiraeConfigGenerator = (): IMoiraeConfig<IPublisherConfig> => {
-  const config: IMoiraeConfig<IPublisherConfig> = {
+const moiraeConfigGenerator = (): IMoiraeConfig<
+  IPublisherConfig,
+  IStoreConfig
+> => {
+  const config: IMoiraeConfig<IPublisherConfig, IStoreConfig> = {
     externalTypes: [NotFoundException],
     publisher: {
       nodeId: "demo-node",
@@ -29,6 +38,14 @@ const moiraeConfigGenerator = (): IMoiraeConfig<IPublisherConfig> => {
         namespaceRoot: "__demo-app__",
         type: "rabbitmq",
       };
+  }
+
+  switch (process.env.STORE_TYPE) {
+    case "typeorm":
+      (config.store as ITypeORMStoreConfig) = {
+        type: "typeorm",
+      };
+      config.imports = [TypeOrmModule.forFeature([EventStore])];
   }
   return config;
 };

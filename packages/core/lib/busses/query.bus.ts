1fcd94fa68c17223cd3c591390fbaa8bd8b95dc0
jes3rk <joseph.schefer@gmail.com>
2022-10-14 14:09:31 +0200

    feat: queries in the current domain run without touching the publisher

--- packages/core/lib/busses/query.bus.ts
+++ packages/core/lib/busses/query.bus.ts
@@ -1,7 +1,9 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { randomUUID } from "crypto";
 import { BaseBus } from "../classes/base.bus";
 import { Explorer } from "../classes/explorer.class";
 import { ObservableFactory } from "../factories/observable.factory";
+import { ExecuteOptions } from "../interfaces/execute-options.interface";
 import { IPublisher } from "../interfaces/publisher.interface";
 import { IQuery } from "../interfaces/query.interface";
 import { PUBLISHER, PublisherRole, QUERY_METADATA } from "../moirae.constants";
@@ -19,5 +21,35 @@ export class QueryBus extends BaseBus<IQuery> {
   ) {
     super(explorer, QUERY_METADATA, observableFactory, publisher);
     this._publisher.role = PublisherRole.QUERY_BUS;
+  }
+
+  /**
+   * Execute the provided query on a remote system
+   */
+  public async execute<TRes>(
+    query: IQuery,
+    options: ExecuteOptions = {},
+  ): Promise<TRes> {
+    const { throwError = false } = options;
+    const _key = randomUUID();
+    query.$responseKey = _key;
+
+    let res: TRes;
+
+    if (!query.$executionDomain) query.$executionDomain = "default";
+    if (query.$executionDomain === this._publisher.domain) {
+      res = (await this.executeLocal(
+        query,
+        options as Record<string, unknown>,
+      )) as TRes;
+    } else {
+      await this._publisher.publish(query);
+      const external = await this._publisher.awaitResponse(_key);
+      if (external.payload instanceof Error && throwError)
+        throw external.payload;
+      res = external.payload as TRes;
+    }
+
+    return res;
   }
 }
